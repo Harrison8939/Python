00f511deaa5fcad0fa555d105ca2cd5750b9edc2
Harrison8939 <56690464+Harrison8939@users.noreply.github.com>
2019-10-17 19:39:06 +0100

    Update list numbers below 5.py

--- list numbers below 5.py
+++ list numbers below 5.py
@@ -1,10 +1,11 @@
-#a is a list of numbers,
+#a is a list of numbers, x is an empty list
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 x = []
 
-
+#tests each number in list a, if the number is below 5, appends them to list x
 for number in a:
     if number < 5:
         x.append(number)
-        
+
+#prints the new list of numbers below 5
 print(x)
