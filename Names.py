cf4f20b66cbf91a9abc85f42c4237a8b8355c0df
Harrison8939 <56690464+Harrison8939@users.noreply.github.com>
2019-10-17 17:34:52 +0100

    Update Names.py

--- Names.py
+++ Names.py
@@ -1,6 +1,23 @@
 #Testing the input function for names
 name = input("Enter your name\n ")
 print ("Your name is " + name)
-#Input function testing for age
-age = input("Enter your age\n ")
+#Input function testing for age, use int(input) for numbers
+age = int(input("Enter your age, use numbers!\n "))
 print ("You are",age, "Years old")
+
+#Different ways to add names together
+
+# use a Plus sign to attach two strings together
+'''
+print("Were" + "wolf")
+print("Door" + "man")
+print("4" + "chan")
+
+#use str(integer) for numbers
+print(str(4) + "chan")
+
+#Multiply a string with an integer
+print(4 * "test")
+'''
+
+
