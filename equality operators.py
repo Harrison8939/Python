0a05758d919fcad22fb3a9652ec9115e563a468c
Harrison8939 <56690464+Harrison8939@users.noreply.github.com>
2019-10-17 19:14:17 +0100

    Testing Equality Operators
    Testing if numbers are equal to 3 or not

---
+++ equality operators.py
@@ -0,0 +1,10 @@
+#Store a number input in variable a
+a = int(input("Enter a number"))
+
+#Test if number is equal to (==) 3
+if a == 3:
+    print("The variable has a value of 3")
+
+#Test if number is not equal to (!=) 3
+elif a != 3:
+    print("The variable does not have the value of 3")
