6db16d8db5b978945ae52228c0b92eff4a91efb8
Harrison8939 <56690464+Harrison8939@users.noreply.github.com>
2019-10-17 19:36:43 +0100

    Update list numbers below 5.py

--- list numbers below 5.py
+++ list numbers below 5.py
@@ -1,9 +1,10 @@
+#a is a list of numbers,
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 x = []
-ask = int(input("Enter a number to test which numbers in the list are lower "))
+
 
 for number in a:
-    if number < ask:
+    if number < 5:
         x.append(number)
         
 print(x)
