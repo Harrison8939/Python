ec0a1f8e4d3042b952e19eebc8e4cdfc5b36ce88
Harrison8939 <56690464+Harrison8939@users.noreply.github.com>
2019-10-17 19:14:38 +0100

    Update Age.py

--- Age.py
+++ Age.py
@@ -5,6 +5,7 @@ age = int(input("enter your age\n"))
 if age > 17:
     print("can see a rated R movie")
 
+
 #test if age is between 17 and 12
 elif age < 17 and age > 12:
     print("Can see a rated PG-13 movie")
